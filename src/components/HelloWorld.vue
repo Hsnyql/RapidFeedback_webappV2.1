8201eb2c9d3c62daf4e2df7c6051aeda33369503
Hsnyql <yuqielu@live.com>
2019-09-10 22:51:52 +1000

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <b-alert
+      :show="dismissCountDown"
+      dismissible
+      variant="warning"
+      @dismissed="dismissCountDown=0"
+      @dismissed-count-down="countDownChanged"
+      >
+      This alert will dismissed in {{ dismissCountDown }} seconds.
+    </b-alert>
+    <b-button @click="showAlert">ShowAlert</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      dismissCountDown: 0,
+      dismissSec: 5
+    }
+  },
+  methods: {
+    showAlert () {
+      this.dismissCountDown = this.dismissSec
+    },
+    countDownChanged (dismissCountDown) {
+      this.dismissCountDown = dismissCountDown
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
