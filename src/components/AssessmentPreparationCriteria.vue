546cfbb78dfc349b946e54121eb55d26bc336fe1
xy-zc <steven9107@gmail.com>
2019-10-13 22:22:24 +1100

    -Modified Component: AssessmentPreparationGrading --Associated a Modal with the Add button for adding new criterion

--- src/components/AssessmentPreparationCriteria.vue
+++ src/components/AssessmentPreparationCriteria.vue
@@ -19,8 +19,26 @@
                 <b-row>
                     <b-col cols="9"><h4>Criteria List</h4><hr></b-col>
                     <b-col>
-                        <b-button variant="primary">Add</b-button>
+                        <b-button v-b-modal.newCriterion variant="primary">Add</b-button>
                     </b-col>
+                    <b-modal id="newCriterion"
+                             ref="modal"
+                             title="Enter New Criterion Name"
+                             @show="resetModal"
+                             @hidden="resetModal"
+                             @ok="recordNewCriterion">
+                        <form ref="form" @submit.stop.prevent="handleSubmit">
+                            <b-form-group :state="nameState"
+                                          label="New Criterion Name:"
+                                          label-for="criterionName-input"
+                                          invalid-feedback="New Criterion Name is Required">
+                                <b-form-input id="criterionName-input"
+                                              v-model="newCriterionName"
+                                              :state="nameState"
+                                              required></b-form-input>
+                            </b-form-group>
+                        </form>
+                    </b-modal>
                 </b-row>
                 <b-row>
                     <b-form-group>
@@ -28,8 +46,8 @@
                             v-for="criterion in criteria"
                             v-model="selected"
                             :key="criterion.id"
-                            :value="criterion.criteriaName">
-                        {{criterion.criteriaName}}
+                            :value="criterion.criterionName">
+                        {{criterion.criterionName}}
                     </b-form-checkbox>
 <!--                        <b-form-checkbox-group-->
 <!--                                v-model="selected"-->
@@ -41,6 +59,12 @@
                 </b-row>
                 <div>
                     Test Only: Selected: <strong>{{ selected }}</strong>
+                </div>
+                <div>
+                    Test Only: Current Criteria:
+                    <div v-for="criterion in criteria"
+                         v-bind:key="criterion.id">
+                        ID: {{criterion.id}}, Name: {{criterion.criterionName}}</div>
                 </div>
                 <b-row align-h="between">
                     <b-col cols="4">
@@ -68,13 +92,41 @@ export default {
   name: 'AssessmentPreparationCriteria',
   data () {
     return {
+      newCriterionName: '',
+      nameState: null,
       selected: [], //  must be an array
       criteria: [
-        {id: 1, criteriaName: 'Criteria 1'},
-        {id: 2, criteriaName: 'Criteria 2'},
-        {id: 3, criteriaName: 'Criteria 3'},
-        {id: 4, criteriaName: 'Criteria 4'}
+        {id: 1, criterionName: 'Criteria 1'},
+        {id: 2, criterionName: 'Criteria 2'},
+        {id: 3, criterionName: 'Criteria 3'},
+        {id: 4, criterionName: 'Criteria 4'}
       ] //  Sample data for display
+    }
+  },
+  methods: {
+    checkFormValidity () {
+      const valid = this.$refs.form.checkValidity()
+      this.nameState = valid ? 'valid' : 'invalid'
+      return valid
+    },
+    resetModal () {
+      this.newCriterionName = ''
+      this.nameState = null
+    },
+    recordNewCriterion (bvModalEvt) {
+      bvModalEvt.preventDefault()
+      this.handleSubmit()
+    },
+    handleSubmit () {
+      if (!this.checkFormValidity()) {
+        return
+      }
+      let newCriterion = {id: this.criteria.length + 1, criterionName: this.newCriterionName}
+      this.criteria.push(newCriterion)
+      this.selected.push(newCriterion)
+      this.$nextTick(() => {
+        this.$refs.modal.hide()
+      })
     }
   }
 }
